307278867aa9fda41f5a27a0402d6b5b3370b455
YuriDevAT <JUndeutsch@gmx.at>
2021-03-23 19:13:41 +0100

    updated function but still bug

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import Image from 'next/image';
-import { bgColor } from './function';
+import { useState } from 'react';
 
-const Home = ({ pokedex }) => {
+const Home = ({ pokemonData }) => {
+    const [justTry, setJustTry] = useState('');
 
     return (
         <Layout title="Pokedex">
@@ -18,11 +19,10 @@ const Home = ({ pokedex }) => {
             </div>
             <h2 className="text-xl mt-8 font-semibold ml-7">Pokédex</h2>
             <ul className="flex flex-wrap justify-around">
-                {pokedex.map((pokemon, index) => (
+                {pokemonData.map((pokemon, index) => (
                     <li key={index} className="w-40">
                         <Link href={`/pokedex?id=${index + 1}`}>
-                            <a className="border p-4 my-2 hover:shadow-md flex flex-col items-center text-sm rounded-md bg-gray-200"
-                            style={{backgroundColor: bgColor(pokemon)}}>
+                            <a className="border p-4 my-2 hover:shadow-md flex flex-col items-center text-sm rounded-md bg-gray-200">
                                 <img
                                     src={pokemon.image}
                                     alt={pokemon.name}
@@ -45,19 +45,52 @@ const Home = ({ pokedex }) => {
 
 export default Home;
 
-export async function getStaticProps(context) {
-    try {
-        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-        const { results } = await res.json();
-        const pokedex = results.map((pokemon, index) => {
-            const paddedId = ('00' + (index + 1)).slice(-3);
-            const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
-            return { ...pokemon, image };
-        });
-        return {
-            props: { pokedex },
-        };
-    } catch (err) {
-        console.error(err);
-    };
+// export async function getStaticProps(context) {
+//     try {
+//         const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+//         const { results } = await res.json();
+//         const pokedex = results.map((pokemon, index) => {
+//             const paddedId = ('00' + (index + 1)).slice(-3);
+//             const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+//             return { ...pokemon, image };
+//         });
+//         return {
+//             props: { pokedex },
+//         };
+//     } catch (err) {
+//         console.error(err);
+//     };
+// }
+
+function getPokemonData() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then((res) => res.json())
+        .then((pokemonData) => {
+            const results = pokemonData.results;
+            results.forEach((results) => {
+                    fetchPokemonData(results)
+            })
+            return {
+                props: { pokemonData },
+            }
+        })
+        .catch((e) => console.log(e))
 }
+getPokemonData();
+
+function fetchPokemonData(pokemon){
+    let url = pokemon.url 
+      fetch(url)
+      .then(response => response.json())
+      .then(function(pokeData){
+          const type = pokeData.types.type;
+          const name = pokeData.name;
+          const paddedId = ('00' + (index + 1)).slice(-3);
+          const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+          return { ...name, image, type };      
+    });
+    return {
+        props: { pokeData },
+    }
+  }
+        // add pokeData to state here, that is what contains all the data you need
